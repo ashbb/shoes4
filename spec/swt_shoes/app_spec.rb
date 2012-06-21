4c54999eccc305754ec55b2da8f80bb6a57efb86
ashbb <ashbbb@gmail.com>
2012-06-21 20:59:10 +0900

    Revised spec/swt_shoes/app_spec.rb. Then passed all tests. Refer to https://github.com/shoes/shoes4/issues/77

--- spec/swt_shoes/app_spec.rb
+++ spec/swt_shoes/app_spec.rb
@@ -1,17 +1,27 @@
 require "swt_shoes/spec_helper"
 
+class TrueClass
+  def x; 0 end
+  def y; 0 end
+end
+
 describe Shoes::Swt::App do
 
   let(:mock_shell) { mock(:swt_shell,
-                     :setSize => true, :setText => true,
+                     :setSize => true, :setText => true, :getSize => true, 
                      :addListener => true, :setLayout => true, 
-                     :setBackground => true, :open => true,
-		     :background_mode= => true,
+                     :setBackground => true, :open => true, :pack => true, 
+                     :background_mode= => true, :addControlListener => true,
                      :set_image => true) }
+                     
+  let(:mock_composite) { mock(:swt_composite,
+                     :setBackground => true, 
+                     :setSize => true, :setLayout => true) }
 
   before :each do
     ::Swt::Widgets::Shell.stub(:new) { mock_shell }
     ::Swt.stub(:event_loop)
+    ::Swt::Widgets::Composite.stub(:new) { mock_composite }
   end
 
   describe Shoes::App do
